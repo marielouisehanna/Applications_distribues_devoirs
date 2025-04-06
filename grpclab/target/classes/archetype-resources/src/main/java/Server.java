e8491317d5e43fa05a9bfe40d39fa7c3020cd401
Marielouise Hanna <130917519+marielouisehanna@users.noreply.github.com>
2025-04-06 16:58:38 +0300

    nothing is working that was my 4th try

---
+++ grpclab/target/classes/archetype-resources/src/main/java/Server.java
@@ -0,0 +1,30 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import td0.MyUser;
+
+public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+            System.out.println("Server is listening on port 12345");
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    MyUser user = MyUser.newBuilder()
+                            .setEmail("john.doe@example.com")
+                            .setYearOfBirth(1980)
+                            .build();
+                    OutputStream outputStream = socket.getOutputStream();
+                    user.writeTo(outputStream);
+                    outputStream.flush();
+                    user.writeTo(new FileOutputStream("td1.ser"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
